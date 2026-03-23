681f8c4555e49ed2f88bd0552b46dab099e88d0c
nicolasmarin <marin.nicolas.ludovic@gmail.com>
2026-03-23 18:37:37 +0100

    fix base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  site: 'https://m4riin.github.io',
-  base: import.meta.env.PROD ? '/elinab' : '/',
+  site: 'https://m4riin.github.io/elinab',
+  base: isProd ? '/' : '/',
   integrations: [tailwind()],
 });
